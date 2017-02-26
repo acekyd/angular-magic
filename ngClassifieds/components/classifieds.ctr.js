c7f2b6302e4d330d3e008ca2ecc3563508a6613b
Abati Adewale <acekyd01@gmail.com>
2017-02-26 16:40:28 +0100

    Adding a new classified

--- ngClassifieds/components/classifieds.ctr.js
+++ ngClassifieds/components/classifieds.ctr.js
@@ -4,11 +4,17 @@
 
 	angular
 		.module("ngClassifieds")
-		.controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav) {
+		.controller("classifiedsCtrl", function($scope, $http, classifiedsFactory, $mdSidenav, $mdToast) {
 
 			classifiedsFactory.getClassifieds().then(function(classifieds) {
 				$scope.classifieds = classifieds.data;
 			})
+
+			var contact = {
+				name: "Ace",
+				phone: "234 706 115 7137",
+				email: "acekyd@ymail.com"
+			}
 
 			$scope.openSidebar = function() {
 				$mdSidenav('left').open();
@@ -16,6 +22,22 @@
 
 			$scope.closeSidebar = function() {
 				$mdSidenav('left').close();
+			}
+
+			$scope.saveClassified = function(classified) {
+				if(classified) {
+					classified.contact = contact;
+					$scope.classifieds.push(classified);
+					$scope.classified = {};
+					$scope.closeSidebar();
+
+					$mdToast.show (
+						$mdToast.simple()
+							.content('Classified saved!')
+							.position('top, right')
+							.hideDelay(3000)
+					);
+				}
 			}
 
 		});
